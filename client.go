f5d3e43d09ebb55f0b938454a2117ccb84bb5eba
Urjit Singh Bhatia <urjitsb87@gmail.com>
2015-10-26 12:41:47 -0700

    Split counters and gauges librato post batch
    Librato servers can't understand the Counter + Gauge json
    data marshalled by this lib when sent together for some reason.
    
    This is a fix that works around this problem till I find a better
    solution.

--- client.go
+++ client.go
@@ -3,8 +3,10 @@ package librato
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 )
 
@@ -65,18 +67,46 @@ type Batch struct {
 	Source      string        `json:"source"`
 }
 
+// Seems like librato doesn't like us sending gauges and counters together
+// Split the structs to send two json batches.
+type GaugeBatch struct {
+	Gauges []Measurement `json:"gauges,omitempty"`
+}
+
+type CounterBatch struct {
+	Counters []Measurement `json:"counters,omitempty"`
+}
+
 func (self *LibratoClient) PostMetrics(batch Batch) (err error) {
+
+	var counterErr error
+	if len(batch.Counters) > 0 {
+		counterErr = self.PostCounters(batch)
+	}
+
+	var gaugeErr error
+	if len(batch.Gauges) > 0 {
+		gaugeErr = self.PostGauges(batch)
+	}
+	if counterErr != nil || gaugeErr != nil {
+		return errors.New(fmt.Sprintf("Metrics post error. Gauge post error: %v Counter post error: %v", gaugeErr, counterErr))
+	}
+	return
+}
+
+func (self *LibratoClient) PostCounters(batch Batch) (err error) {
 	var (
 		js   []byte
 		req  *http.Request
 		resp *http.Response
 	)
 
-	if len(batch.Counters) == 0 && len(batch.Gauges) == 0 {
-		return nil
+	for i := 0; i < len(batch.Counters); i++ {
+		batch.Counters[i]["source"] = batch.Source
+		batch.Counters[i]["measure_time"] = batch.MeasureTime
 	}
-
-	if js, err = json.Marshal(batch); err != nil {
+	counterBatch := CounterBatch{batch.Counters}
+	if js, err = json.Marshal(counterBatch); err != nil {
 		return
 	}
 
@@ -88,16 +118,60 @@ func (self *LibratoClient) PostMetrics(batch Batch) (err error) {
 	req.SetBasicAuth(self.Email, self.Token)
 
 	if resp, err = http.DefaultClient.Do(req); err != nil {
+		log.Printf("Error Return. %s\n", err)
 		return
 	}
-	defer resp.Body.Close()
 
+	var body []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		body = []byte(fmt.Sprintf("(could not fetch response body for error: %s)", err))
+	}
 	if resp.StatusCode != http.StatusOK {
-		var body []byte
-		if body, err = ioutil.ReadAll(resp.Body); err != nil {
-			body = []byte(fmt.Sprintf("(could not fetch response body for error: %s)", err))
-		}
 		err = fmt.Errorf("Unable to post to Librato: %d %s %s", resp.StatusCode, resp.Status, string(body))
 	}
+	log.Printf("Response from counter data post to Librato: %d %s %s\n", resp.StatusCode, resp.Status, string(body))
+	resp.Body.Close()
+
+	return
+}
+
+func (self *LibratoClient) PostGauges(batch Batch) (err error) {
+	var (
+		js   []byte
+		req  *http.Request
+		resp *http.Response
+	)
+
+	for i := 0; i < len(batch.Gauges); i++ {
+		batch.Gauges[i]["source"] = batch.Source
+		batch.Gauges[i]["measure_time"] = batch.MeasureTime
+	}
+
+	gaugeBatch := GaugeBatch{batch.Gauges}
+	if js, err = json.Marshal(gaugeBatch); err != nil {
+		return
+	}
+	if req, err = http.NewRequest("POST", MetricsPostUrl, bytes.NewBuffer(js)); err != nil {
+		return
+	}
+
+	req.Header.Set("Content-Type", "application/json")
+	req.SetBasicAuth(self.Email, self.Token)
+
+	if resp, err = http.DefaultClient.Do(req); err != nil {
+		log.Printf("Error Return. %s\n", err)
+		return
+	}
+
+	var body []byte
+	if body, err = ioutil.ReadAll(resp.Body); err != nil {
+		body = []byte(fmt.Sprintf("(could not fetch response body for error: %s)", err))
+	}
+	if resp.StatusCode != http.StatusOK {
+		err = fmt.Errorf("Unable to post to Librato: %d %s %s", resp.StatusCode, resp.Status, string(body))
+	}
+	log.Printf("Response from gauge data post to Librato: %d %s %s\n", resp.StatusCode, resp.Status, string(body))
+	resp.Body.Close()
+
 	return
 }
